6a304fa017e35814f3bbd6505e02b737a7f984a6
Inosha <inoshas@gmail.com>
2023-04-17 15:30:27 +0300

    create and update table

---
+++ main_code.py
@@ -0,0 +1,4 @@
+import tables_and_data_DB2
+
+tables_and_data_DB2.insert_values()
+print("done")
---
+++ tables_and_data_DB2.py
@@ -0,0 +1,53 @@
+import sqlite3
+#import csv
+
+db = sqlite3.connect('expenses_account.db')
+cur = db.cursor()
+
+
+#### Create table here:: 
+### Table: local_transport
+## Columns: id, year, month, date, location, distance_km, price_per_km, total_price
+cur.execute("""
+CREATE TABLE IF NOT EXISTS local_transport (
+    id INTEGER PRIMARY KEY,
+    year INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    date INTEGER NOT NULL,
+    location TEXT NOT NULL,
+    distance_km REAL NOT NULL,
+    price_per_km REAL NOT NULL,
+    total_price_euro REAL 
+)
+""")
+
+
+#### Insert colums if something missing: 
+''' 
+cur.execute(""" ALTER TABLE local_transport
+    ADD COLUMN month Integer """)
+'''
+
+def insert_values():
+    db = sqlite3.connect('expenses_account.db')
+    cur = db.cursor()
+    data= input("Add  year, month, date, location, distance_km, price_per_km, total_price_euro in correct order and seperate with ',' : ")
+    list_data=data.split(",") 
+    cur.execute("INSERT INTO local_transport (year, month, date, location, distance_km, price_per_km, total_price_euro) VALUES (?,?,?,?,?,?,?)", 
+                (list_data[0],list_data[1],list_data[2],list_data[3],list_data[4],list_data[5], int(list_data[5])*int(list_data[4]) ))
+    print("endQQQ")
+    db.commit()
+    db.close()
+
+#insert_values()
+
+
+"""
+cur.execute('SELECT * from local_transport')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+"""
+db.commit()
+db.close()
+
