e7e1dd8697539b061bf7c89327f1a270de0c293b
Inosha <inoshas@gmail.com>
2023-04-21 12:52:07 +0300

    General code to add data t main

--- create_table/__init__.py
+++ create_table/__init__.py
@@ -1,36 +1,56 @@
 import sqlite3
 import csv
 
+class cre_tab:
+    db = None
+    cur = None
+    db_file = None
+    file_name=None
+    list_data=None
+    
+    #def __init__(self, db_file=":memory:"):
+    def __init__(self, db_file):
+        self.db_file=db_file
+        self.db = sqlite3.connect(db_file)
+        self.cur = self.db.cursor()
+    
+    def __del__(self):
+        self.db.close()
 
-class create_table:
-    # identify DB based on the requirement
-    def identify_DB(type):
-        if type== "products":
-            db = sqlite3.connect('Inventory_and_price.db')
-            cur = db.cursor()
-            
-        elif type=="expenses":
-            db = sqlite3.connect('expenses_account.db')
-            cur = db.cursor()
-            
-        # add values to table based on the table name and method    
-    def add_table_val(table_name, adding_method):
+    def commit(self):
+        self.db.commit()
+
+
+# identify DB based on the requirement 
+    # add values to table based on the table name and method    
+    def add_table_val(self, adding_method, table_name, file_name,list_data):
         if adding_method =="file":
-            create_table.read_csvfile(file_name=="")
-            create_table.add_data(data_array="")
+            self.read_csvfile(table_name,file_name)
         elif adding_method=="manually":
-            create_table.add_data()
+            self.add_data(table_name,list_data)
                 
     
-    def read_csvfile(file_name):
+    def read_csvfile(self,table_name, file_name):
         with open(file_name, 'r') as csv_file:
-        reader = csv.reader(csv_file, delimiter=';')
-        next(reader)
+            reader = csv.reader(csv_file, delimiter=';')
+            next(reader) 
+            for row in reader: 
+                list_data=row[0]
+                self.add_data(table_name,list_data)
+           
         
-        for row in reader: 
-            list_data=row[0].split(",")
-            return list_data
+    def add_data(self, table_name,list_data):
+      ## This will define which data to which table and how.....   
+        if table_name== "phone_bills":
+            insert_sql=f""" INSERT INTO phonebills (year, month, date, invoice_value, VAT_presentage) VALUES (?,?,?,?,?)"""
+            self.cur.execute(insert_sql, (list_data[0],list_data[1],list_data[2],list_data[3],list_data[4] ))
+          #  (list_data[0],list_data[1],list_data[2],list_data[3],list_data[4] )) """
+                
+        elif table_name=="transport":
+            insert_sql=f"""INSERT INTO local_transport (year, month, date, location, distance_km, price_per_km, total_price_euro) VALUES (?,?,?,?,?,?,?) """
+            self.cur.execute(insert_sql, (list_data[0],list_data[1],list_data[2],list_data[3],list_data[4],list_data[5], int(list_data[5])*int(list_data[4]) ))
+
+        ##### Here need to add all files and its entities......
         
-    def add_data():
-        r=3
+        self.commit()
         
